5ceac335fc8e8894b05aa0ff6e93a126849d3b25
Ty <tylersloane@gmail.com>
2025-03-22 10:20:01 -0400

    Add password validation on registration via stimulus.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import PasswordValidationController from "./password_validation_controller"
+application.register("password-validation", PasswordValidationController)
---
+++ app/javascript/controllers/password_validation_controller.js
@@ -0,0 +1,48 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="password-validation"
+export default class extends Controller {
+  static targets = ["password", "confirmation",  "submitButton"]
+
+  connect() {
+    console.log("hiE!D")
+    this.validateOnInput()
+  }
+
+  validateOnInput() {
+    // Check on confirmation input
+    this.confirmationTarget.addEventListener("input", () => {
+      this.validatePasswordMatch()
+    })
+
+    // Check when password changes and confirmation has content
+    this.passwordTarget.addEventListener("input", () => {
+      if (this.confirmationTarget.value.length > 0) {
+        this.validatePasswordMatch()
+      }
+    })
+  }
+
+  validatePasswordMatch() {
+    const passwordsMatch = this.passwordTarget.value === this.confirmationTarget.value
+    console.log("passwords match", this.passwordTarget.value, this.confirmationTarget.value)
+
+    if (passwordsMatch) {
+      // this.errorTarget.classList.add("hidden")
+      this.confirmationTarget.classList.remove("input-error")
+      this.submitButtonTarget.disabled = false
+    } else {
+      // this.errorTarget.classList.remove("hidden")
+      this.confirmationTarget.classList.add("input-error")
+      this.submitButtonTarget.disabled = true
+    }
+
+    return passwordsMatch
+  }
+
+  validateBeforeSubmit(event) {
+    if (!this.validatePasswordMatch()) {
+      event.preventDefault()
+    }
+  }
+}
